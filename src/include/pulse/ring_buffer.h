d0b3a97b1e8ede284ac81c9f67d19c124fbdad26
Artyom Lebedev <vagran.ast@gmail.com>
2026-04-12 20:16:27 +0300

    Ring buffer.

---
+++ src/include/pulse/ring_buffer.h
@@ -0,0 +1,191 @@
+#ifndef RING_BUFFER_H
+#define RING_BUFFER_H
+
+#include <pulse/details/common.h>
+#include <etl/span.h>
+
+
+namespace pulse {
+
+namespace details {
+
+template <typename T, size_t bufferSize, etl::unsigned_integral TIndex>
+class RingBufferBase {
+protected:
+    etl::array<T, bufferSize> buffer;
+    static constexpr TIndex size = bufferSize;
+
+    static_assert(bufferSize > 0);
+    static_assert(PULSE_IS_POW2(bufferSize));
+    static_assert(bufferSize * 2 <= etl::numeric_limits<TIndex>::max());
+
+    T *
+    GetBuffer()
+    {
+        return buffer.data();
+    }
+};
+
+template <typename T, etl::unsigned_integral TIndex>
+class RingBufferBase<T, etl::dynamic_extent, TIndex> {
+public:
+    RingBufferBase() = delete;
+
+    RingBufferBase(T *buffer, TIndex size):
+        buffer(buffer),
+        size(size)
+    {
+        PULSE_ASSERT(size > 0);
+        PULSE_ASSERT(PULSE_IS_POW2(size));
+        PULSE_ASSERT(size * 2 <= etl::numeric_limits<TIndex>::max());
+    }
+
+protected:
+    T *buffer;
+    const TIndex size;
+
+    T *
+    GetBuffer()
+    {
+        return buffer;
+    }
+};
+
+} // namespace details
+
+
+/** Light-weight ring buffer implementation. Buffer size should be strictly power of two to enable
+ * fast math.
+ * @tparam T Type of values to store.
+ * @tparam TIndex Type of index field.
+ */
+template <typename T, size_t bufferSize = etl::dynamic_extent,
+          etl::unsigned_integral TIndex = pulse::SizedUint<pulse::UintBitWidth(
+            bufferSize == etl::dynamic_extent ? etl::dynamic_extent : bufferSize * 2)>>
+class RingBuffer: public details::RingBufferBase<T, bufferSize, TIndex> {
+public:
+    using details::RingBufferBase<T, bufferSize, TIndex>::RingBufferBase;
+
+    /**
+     * @return TIndex Number of elements currently contained in the buffer.
+     */
+    TIndex
+    GetSize() const
+    {
+        return writePos - readPos;
+    }
+
+    /**
+     * @return TIndex Number of elements still can write into the buffer.
+     */
+    TIndex
+    GetAvailableCapacity() const
+    {
+        return this->size - GetSize();
+    }
+
+    bool
+    IsEmpty() const
+    {
+        return GetSize() == 0;
+    }
+
+    bool
+    IsFull() const
+    {
+        return GetAvailableCapacity() == 0;
+    }
+
+    /** Write data into the buffer, not more than currently available capacity.
+     * @return TIndex Actually added data size.
+     */
+    TIndex
+    Write(const T *data, TIndex size)
+    {
+        size = etl::min(size, GetAvailableCapacity());
+        TIndex bufferWritePos = writePos & (this->size - 1);
+        TIndex tailSize = etl::min<TIndex>(size, this->size - bufferWritePos);
+        etl::copy(data, data + tailSize, this->GetBuffer() + bufferWritePos);
+        if (tailSize < size) {
+            etl::copy(data + tailSize, data + size, this->GetBuffer());
+        }
+        writePos += size;
+        return size;
+    }
+
+    /**
+     * @return TIndex Actually added data size.
+     */
+    TIndex
+    Write(etl::span<const T> data)
+    {
+        return Push(data.data(), data.size());
+    }
+
+    /** Pop data from the buffer.
+     * @return TIndex Number of actually obtained elements.
+     */
+    TIndex
+    Read(T *out, TIndex size)
+    {
+        size = etl::min(size, GetSize());
+        TIndex bufferReadPos = readPos & (this->size - 1);
+        TIndex tailSize = etl::min<TIndex>(size, this->size - bufferReadPos);
+        const T *readPtr = this->GetBuffer() + bufferReadPos;
+        etl::copy(readPtr, readPtr + tailSize, out);
+        if (tailSize < size) {
+            etl::copy(this->GetBuffer(), this->GetBuffer() + size - tailSize, out + tailSize);
+        }
+        readPos += size;
+        return size;
+    }
+
+    /** Get next available write region. Returns empty region if full. Write should be committed by
+     * subsequent CommitWrite() call.
+     */
+    etl::span<T>
+    GetWriteRegion()
+    {
+        TIndex bufferWritePos = writePos & (this->size - 1);
+        TIndex size = etl::min<TIndex>(GetAvailableCapacity(), this->size - bufferWritePos);
+        T *writePtr = this->GetBuffer() + bufferWritePos;
+        return {writePtr, writePtr + size};
+    }
+
+    /** Commit write to a region previously obtained by GetWriteRegion(). */
+    void
+    CommitWrite(TIndex size)
+    {
+        PULSE_ASSERT(size <= etl::min<TIndex>(GetAvailableCapacity(),
+                                              this->size - (writePos & (this->size - 1))));
+        writePos += size;
+    }
+
+    /** Get next available read region. Returns empty region if empty. Read should be committed by
+     * subsequent CommitRead() call.
+     */
+    etl::span<const T>
+    GetReadRegion()
+    {
+        TIndex bufferReadPos = readPos & (this->size - 1);
+        TIndex size = etl::min<TIndex>(GetSize(), this->size - bufferReadPos);
+        T *readPtr = this->GetBuffer() + bufferReadPos;
+        return {readPtr, readPtr + size};
+    }
+
+    /** Commit read to a region previously obtained by GetReadRegion(). */
+    void
+    CommitRead(TIndex size)
+    {
+        PULSE_ASSERT(size <= etl::min<TIndex>(GetSize(),
+                                              this->size - (readPos & (this->size - 1))));
+        readPos += size;
+    }
+
+private:
+    TIndex writePos = 0, readPos = 0;
+};
+
+} // namespace pulse
+
+#endif /* RING_BUFFER_H */
---
+++ test/test_ring_buffer.cpp
@@ -0,0 +1,193 @@
+#include <catch2/catch_test_macros.hpp>
+#include <pulse/ring_buffer.h>
+
+using namespace pulse;
+
+
+TEST_CASE("RingBuffer basic push/pop")
+{
+    RingBuffer<int, 8> rb;
+
+    int input[] = {1, 2, 3, 4};
+    int output[4] = {};
+
+    REQUIRE(rb.GetSize() == 0);
+
+    auto pushed = rb.Write(input, 4);
+    REQUIRE(pushed == 4);
+    REQUIRE(rb.GetSize() == 4);
+
+    auto popped = rb.Read(output, 4);
+    REQUIRE(popped == 4);
+    REQUIRE(rb.GetSize() == 0);
+
+    REQUIRE(std::equal(output, output + 4, input));
+}
+
+TEST_CASE("RingBuffer capacity limit")
+{
+    RingBuffer<int, 8> rb;
+
+    int input[10] = {};
+
+    auto pushed = rb.Write(input, 10);
+    REQUIRE(pushed == 8);
+    REQUIRE(rb.GetSize() == 8);
+    REQUIRE(rb.GetAvailableCapacity() == 0);
+}
+
+TEST_CASE("RingBuffer wraparound behavior")
+{
+    RingBuffer<int, 8> rb;
+
+    int input1[] = {1,2,3,4,5};
+    int input2[] = {6,7,8};
+    int output[8] = {};
+
+    rb.Write(input1, 5);
+    rb.Read(output, 3); // advance readPos
+
+    rb.Write(input2, 3); // should wrap
+
+    REQUIRE(rb.GetSize() == 5);
+
+    rb.Read(output, 5);
+
+    int expected[] = {4,5,6,7,8};
+    REQUIRE(std::equal(output, output + 5, expected));
+}
+
+TEST_CASE("RingBuffer partial pop")
+{
+    RingBuffer<int, 8> rb;
+
+    int input[] = {1,2,3};
+    int output[5] = {};
+
+    rb.Write(input, 3);
+
+    auto popped = rb.Read(output, 5);
+    REQUIRE(popped == 3);
+    REQUIRE(rb.GetSize() == 0);
+
+    REQUIRE(std::equal(output, output + 3, input));
+}
+
+TEST_CASE("RingBuffer zero-copy write region")
+{
+    RingBuffer<int, 8> rb;
+
+    auto region = rb.GetWriteRegion();
+    REQUIRE(region.size() == 8);
+
+    for (size_t i = 0; i < region.size(); ++i)
+        region[i] = static_cast<int>(i + 1);
+
+    rb.CommitWrite(8);
+
+    REQUIRE(rb.GetSize() == 8);
+}
+
+TEST_CASE("RingBuffer zero-copy read region")
+{
+    RingBuffer<int, 8> rb;
+
+    int input[] = {1,2,3,4};
+    rb.Write(input, 4);
+
+    auto region = rb.GetReadRegion();
+    REQUIRE(region.size() == 4);
+
+    REQUIRE(region[0] == 1);
+    REQUIRE(region[3] == 4);
+
+    rb.CommitRead(4);
+    REQUIRE(rb.GetSize() == 0);
+}
+
+TEST_CASE("RingBuffer zero-copy wrap split")
+{
+    RingBuffer<int, 8> rb;
+
+    int input1[] = {1,2,3,4,5};
+    rb.Write(input1, 5);
+
+    int tmp[3];
+    rb.Read(tmp, 3); // force wrap scenario
+
+    auto region = rb.GetWriteRegion();
+
+    // Should only expose tail until end of buffer
+    REQUIRE(region.size() == 3);
+
+    for (size_t i = 0; i < region.size(); ++i)
+        region[i] = static_cast<int>(10 + i);
+
+    rb.CommitWrite(region.size());
+
+    REQUIRE(rb.GetSize() == 5);
+}
+
+TEST_CASE("RingBuffer zero-copy read wrap split", "[single]")
+{
+    RingBuffer<int, 8> rb;
+
+    int input1[] = {1,2,3,4,5};
+    int input2[] = {6,7,8, 9,10,11};
+
+    rb.Write(input1, 5);
+
+    int tmp[3];
+    rb.Read(tmp, 3); // advance readPos
+
+    rb.Write(input2, 6); // wrap write
+
+    auto region = rb.GetReadRegion();
+
+    // Should read only contiguous part
+    REQUIRE(region.size() == 5);
+    REQUIRE(std::equal(region.begin(), region.end(), std::vector<int>{4, 5, 6, 7, 8}.begin()));
+    REQUIRE(region[0] == 4);
+    REQUIRE(region[1] == 5);
+
+    rb.CommitRead(region.size());
+
+    REQUIRE(rb.GetSize() == 3);
+}
+
+TEST_CASE("RingBuffer multiple region iteration")
+{
+    RingBuffer<int, 8> rb;
+
+    int input[] = {1,2,3,4,5,6};
+    rb.Write(input, 6);
+
+    int output[6];
+    size_t offset = 0;
+
+    while (rb.GetSize() > 0) {
+        auto region = rb.GetReadRegion();
+        std::copy(region.begin(), region.end(), output + offset);
+        offset += region.size();
+        rb.CommitRead(region.size());
+    }
+
+    REQUIRE(offset == 6);
+    REQUIRE(std::equal(output, output + 6, input));
+}
+
+TEST_CASE("RingBuffer interleaved push/pop")
+{
+    RingBuffer<int, 8> rb;
+
+    for (int i = 0; i < 100; ++i) {
+        rb.Write(&i, 1);
+
+        int out;
+        rb.Read(&out, 1);
+
+        REQUIRE(out == i);
+    }
+
+    REQUIRE(rb.GetSize() == 0);
+}
